4ba59790af565ada6bc942b5b458aece142c8670
jktemple <jktemple@ucsc.edu>
2025-10-21 15:13:18 -0700

    adding new files and changing things

--- src/data.ts
+++ src/data.ts
@@ -1,22 +1,22 @@
 import { AboutMeData, Game, GameEngine, LinkImageSource, MediaType, Platform } from "./types";
 
 export const PersonalInfo: AboutMeData = {
-  name: "YOUR_NAME",
-  role: "YOUR_ROLE",
+  name: "Josh Temple",
+  role: "Generalist Game Programmer and Technical Designer",
   introduction: "SHORT_INTRODUCTION",
   description: "TELL_ABOUT_YOURSELF",
   image: `${process.env.PUBLIC_URL}/images/Profile.jpg`,
   cvUri: `${process.env.PUBLIC_URL}/files/YOUR_CV.pdf`,
   links: {
-    github: "https://github.com/NAME",
-    itchIO: "https://NAME.itch.io",
-    linkedIn: "https://www.linkedin.com/in/NAME",
+    github: "https://github.com/jktemple",
+    itchIO: "https://jktemple.itch.io",
+    linkedIn: "https://www.linkedin.com/in/josh-temple-50bb181a9",
   }
 };
 
 export const games: Game[] = [
   {
-    name: "GAME_NAME",
+    name: "TurnStyle",
     description: "GAME_DESCRIPTION",
     genres: ["GAME_GENRE", "GAME_GENRE..."],
     platforms: [Platform.Windows],
@@ -34,7 +34,7 @@ export const games: Game[] = [
     ],
   },
   {
-    name: "GAME_NAME",
+    name: "Untitled Parkour Game",
     description: "GAME_DESCRIPTION",
     genres: ["GAME_GENRE", "GAME_GENRE..."],
     platforms: [Platform.Windows],
@@ -44,7 +44,7 @@ export const games: Game[] = [
       { source: LinkImageSource.ItchIo, url: "https://YOUR_ITCH.itch.io/GAME_NAME" },
     ],
     media: [
-      { source: "/images/games/GAME_NAME/Screenshot_1.png", type: MediaType.Image },
+      { source: "/images/games/UntitledParkourGame/UntitledParkourGame_Gif1", type: MediaType.Image },
       { source: "https://www.youtube.com/embed/dQw4w9WgXcQ", type: MediaType.YouTube },
       { source: "/images/games/GAME_NAME/Screenshot_2.png", type: MediaType.Image },
       { source: "/images/games/GAME_NAME/Screenshot_3.png", type: MediaType.Image },
